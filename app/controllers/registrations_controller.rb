d24f8d2d3b39223f82cb1d1b84f5106a880e3c04
laterDays <dadamszx@gmail.com>
2015-12-17 11:48:55 -0500

    working on no browser auth

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,12 @@
+class RegistrationsController < Devise::RegistrationsController
+
+  def create
+    @user = User.create(params[:user])
+    if @user.save
+      render :json => {:state => {:code => 0}, :data => @user }
+    else
+      render :json => {:state => {:code => 1, :messages => @user.errors.full_messages} }
+    end
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   root 'users#index'
 
   use_doorkeeper
-  devise_for :users
+  
+  devise_for :users, :controllers => {:registrations => "registrations"}
   
   #api
   namespace :api do
