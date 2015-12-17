1ad2c282f7252fce7fff5f1a75abe621fc4e5269
laterDays <dadamszx@gmail.com>
2015-12-17 13:30:31 -0500

    i want to see server-side info

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
   skip_before_filter  :verify_authenticity_token
   def create
+  	logger.info params
     @user = User.create(params[:user])
     if @user.save
       render :json => {:state => {:code => 0}, :data => @user }
