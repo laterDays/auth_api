c22a90f505aceca50f57d4f314f9259a302d39e8
laterDays <dadamszx@gmail.com>
2015-12-17 13:49:24 -0500

    my params

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,13 +1,19 @@
 class RegistrationsController < Devise::RegistrationsController
   skip_before_filter  :verify_authenticity_token
   def create
-  	logger.info "[ ] RegistrationsController.create() params: " + params.to_json
-    @user = User.create(params[:user])
+  	logger.info "[ ] RegistrationsController.create() params: " + user_params.to_json
+    @user = User.create(user_params[:user])
     if @user.save
       render :json => {:state => {:code => 0}, :data => @user }
     else
       render :json => {:state => {:code => 1, :messages => @user.errors.full_messages} }
     end
 
+  end
+
+  private
+
+  def user_params
+  	params.require(:user).permit(:email, :password)
   end
 end
